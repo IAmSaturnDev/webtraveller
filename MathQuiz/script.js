0452464aad6117da79f3b37b6109ed448623216d
RandomWater <86333848+RandomWater@users.noreply.github.com>
2022-10-04 14:03:15 -0600

    EPIC UPDATE

---
+++ MathQuiz/script.js
@@ -0,0 +1,33 @@
+let answer
+
+answer = prompt("What is 2 * 2?")
+if (answer == "4"){
+    alert("Correct!")
+}else{
+    alert("False!")
+}
+answer = prompt("What is 4 + 6?")
+if (answer == "10"){
+    alert("Correct!")
+}else{
+    alert("False!")
+}
+answer = prompt("X + 8 = 12 What is the value of X?")
+if (answer == "4"){
+    alert("Correct!")
+}else{
+    alert("False!")
+}
+answer = prompt("X - Y = 0 What are x and y?")
+if (answer == "anything"){
+    alert("Correct!")
+}else{
+    alert("False!")
+}
+answer = prompt("What is 6 / 2?")
+if (answer == "3"){
+    alert("Correct!")
+}else{
+    alert("False!")
+}
+alert("You did the quiz hooray!")
